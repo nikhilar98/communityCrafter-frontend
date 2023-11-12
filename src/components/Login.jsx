d813894cb6ad32b19ae55c53409f78e2afb0adc7
nikhilar98 <nikhilar98@gmail.com>
2023-11-12 23:53:34 +0530

    updates

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,29 @@
 import LoginForm from "./LoginForm"
+import { useLocation } from "react-router-dom"
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login(){ 
+
+    const location = useLocation()
+
+    const notify = () => toast("Registered Successfully. Check your mail to Login!");
 
     function handleSubmit(e){
         e.preventDefault()
     }
 
+    console.log(location.state?.msg)
+    if(location.state){
+        notify()
+    }
+
+
 
     return (
         <div className="setBackgroundImage">
+            <ToastContainer />
            <LoginForm/>
         </div>
     )
--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -6,10 +6,14 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import FormHelperText from '@mui/material/FormHelperText';
 import { createTheme,ThemeProvider  } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import axios from '../axios/axios';
+import {useFormik} from 'formik'
+import * as Yup from 'yup'
+
 
 const theme = createTheme({
   palette: {
@@ -24,50 +28,90 @@ const theme = createTheme({
 
 export default function RegistrationForm() {
 
-    const [username,setUsername] = useState('')
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
-    const [phone,setPhone] = useState('')
-    const [role,setRole] = useState('')
+   const [serverErrors,setServerErrors] = useState([])
+   const navigate = useNavigate()
+    // async function handleSubmit(e){ 
+    //     e.preventDefault()
+    //     const formData= { 
+    //         username,
+    //         email,
+    //         password,
+    //         phone,
+    //         role
+    //     }
+    //     try{
+    //       const response = await axios.post('/comcraft/register',formData)
+    //       console.log(response.data)
+    //     }
+    //     catch(err){
+    //       console.log(err)
+    //     }
+        
+    // }
+    const userValidationSchema = Yup.object().shape({
+      username:Yup.string().required('username is required.'),
+      email:Yup.string().required('email is required.').email('Invalid email'),
+      password:Yup.string().required('Password is required'),
+      phone:Yup.string().required('Phone is required').length(10,'Phone must have 10 digits'),
+      role:Yup.string().required('Role is required')
+  })
 
-    async function handleSubmit(e){ 
-        e.preventDefault()
-        const formData= { 
-            username,
-            email,
-            password,
-            phone,
-            role
-        }
+  const formik = useFormik({
+      initialValues : {
+        username:'',
+        email:'',
+        password:'',
+        phone:'',
+        role:''
+      },
+      validationSchema:userValidationSchema,
+      validateOnChange:false,
+      validateOnBlur:false,
+      onSubmit:async (values,{resetForm})=>{
         try{
-          const response = await axios.post('/comcraft/register',formData)
-          console.log(response.data)
+          const response = await axios.post('/comcraft/register',values)
+          navigate('/login',{state:{msg:response.data.msg}})
+          setServerErrors([])
         }
         catch(err){
-          console.log(err)
+          setServerErrors(err.response.data.errors)
         }
-        
-    }
+          
+      }
+  })
+
+  // useEffect(()=>{
+  //     formik.setFieldValue('title',blog ? blog.title : '')
+  //     formik.setFieldValue('content',blog ? blog.content : '')
+  // },[])
 
     return (
       <ThemeProvider theme={theme}>
-        <Box backgroundColor="white" borderRadius="20px" padding="20px" width="500px" onSubmit={handleSubmit} component="form" sx={{'& > :not(style)': { m: 1, width: '25ch' }}} noValidate autoComplete="off">
+        <Box backgroundColor="white" borderRadius="20px" padding="20px" width="500px" onSubmit={formik.handleSubmit} component="form" sx={{'& > :not(style)': { m: 1, width: '25ch' }}} noValidate autoComplete="off">
           <h2>Register with us</h2>
-          <TextField color="customBlue" id="username" label="username" variant="filled" type='text' value={username} onChange={(e)=>{setUsername(e.target.value)}}/><br/>
-          <TextField color="customBlue" id="email" label="email" variant="filled" type='text' value={email} onChange={(e)=>{setEmail(e.target.value)}}/><br/>
-          <TextField color="customBlue" id="password" label="password" variant="filled" type='password' value={password} onChange={(e)=>{setPassword(e.target.value)}}/><br/>
-          <TextField color="customBlue" id="phone" label="phone" variant="filled" type='text' value={phone} onChange={(e)=>{setPhone(e.target.value)}}/><br/>
-          <FormControl color="customBlue">
+
+          <TextField color="customBlue" name="username" id="username" label={formik.errors.username || serverErrors.find(ele=>ele.path=='username')? "Error":"username"} variant="filled" type='text' value={formik.username} onChange={formik.handleChange} error={Boolean(formik.errors.username) || Boolean(serverErrors.find(ele=>ele.path=='username'))} helperText={(formik.errors.username && formik.errors.username) || (serverErrors.find(ele=>ele.path=='username') && serverErrors.find(ele=>ele.path=='username').msg)}/><br/>
+
+          <TextField color="customBlue" name="email" id="email" label={formik.errors.email || serverErrors.find(ele=>ele.path=='email') ? "Error":"email"} variant="filled" type='text' value={formik.email} onChange={formik.handleChange} error={Boolean(formik.errors.email)|| Boolean(serverErrors.find(ele=>ele.path=='email'))} helperText={(formik.errors.email && formik.errors.email) || (serverErrors.find(ele=>ele.path=='email') && serverErrors.find(ele=>ele.path=='email').msg)}/><br/>
+
+          <TextField color="customBlue" name="password" id="password" label={formik.errors.password || serverErrors.find(ele=>ele.path=='password') ? "Error":"password"} variant="filled" type='password' value={formik.password} onChange={formik.handleChange} error={Boolean(formik.errors.password)|| Boolean(serverErrors.find(ele=>ele.path=='password'))} helperText={(formik.errors.password && formik.errors.password) || (serverErrors.find(ele=>ele.path=='password') && serverErrors.find(ele=>ele.path=='password').msg)}/><br/>
+
+          <TextField color="customBlue" name="phone" id="phone" label={formik.errors.phone || serverErrors.find(ele=>ele.path=='phone') ? "Error":"phone"} variant="filled" type='text' value={formik.phone} onChange={formik.handleChange} error={Boolean(formik.errors.phone)|| Boolean(serverErrors.find(ele=>ele.path=='phone'))} helperText={(formik.errors.phone && formik.errors.phone)|| (serverErrors.find(ele=>ele.path=='phone') && serverErrors.find(ele=>ele.path=='phone').msg)}/><br/>
+
+          <FormControl color="customBlue" variant="standard" error={Boolean(formik.errors.role)|| Boolean(serverErrors.find(ele=>ele.path=='role'))}>
             <FormLabel id="roles">What role would you like to join as?</FormLabel>
             <RadioGroup
               aria-labelledby="roles"
               row
-              value={role}
-              onChange={(e)=>{setRole(e.target.value)}}
+              value={formik.role}
+              onChange={formik.handleChange}
+              id="role"
+              name="role"
             >
               <FormControlLabel value="teacher" control={<Radio />} label="Tutor" />
               <FormControlLabel value="communityHead" control={<Radio />} label="Community Head" />
             </RadioGroup>
+            <FormHelperText>{(formik.errors.role && formik.errors.role)|| (serverErrors.find(ele=>ele.path=='role') && serverErrors.find(ele=>ele.path=='role').msg)}</FormHelperText>
           </FormControl><br/>
           <Button id="submit" variant="contained" size='large' type='submit' color="customYellow">Register</Button>
           <p>Already registered? <Link to='/login'>Login</Link></p>
