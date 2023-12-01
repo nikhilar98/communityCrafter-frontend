6d2c4a5a0e08d17080278c74609464428b0b387d
nikhilar98 <nikhilar98@gmail.com>
2023-12-01 12:23:56 +0530

    home page responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,8 +14,8 @@ export default function Home(){
         <>
         <div className="setBackgroundImageHome">
             <h1 style={{marginTop:30,color:'white'}}>Welcome to <span style={{color:'rgb(226, 225, 130)'}}>Community Crafter</span></h1>
-            <p style={{width:'1000px'}}> We prioritize the sense of community. Communities can effortlessly post class requirements, and tutors can seamlessly connect with these opportunities, fostering collaboration and enriching educational experiences.</p>
-            <p style={{width:'1000px',marginBottom:40}}>
+            <p style={{maxWidth:'1000px'}}> We prioritize the sense of community. Communities can effortlessly post class requirements, and tutors can seamlessly connect with these opportunities, fostering collaboration and enriching educational experiences.</p>
+            <p style={{maxWidth:'1000px',marginBottom:40}}>
             We believe in the transformative power of education and community, and our platform is designed to bring together passionate individuals who share a love for teaching and learning.
             </p>
             <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
