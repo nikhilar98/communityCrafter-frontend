d8ce6b9e29586170267e1bcc60755b9d3b8fd2e0
nikhilar98 <nikhilar98@gmail.com>
2023-12-11 18:48:13 +0530

    unused function deleted

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import LoginForm from "./LoginForm"
 import { useLocation } from "react-router-dom"
 import { ToastContainer, toast } from 'react-toastify';
-
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from "./Footer";
 
@@ -11,10 +10,6 @@ export default function Login(){
     const location = useLocation()
 
     const notify = (msg) => toast.success(msg);
-
-    function handleSubmit(e){
-        e.preventDefault()
-    }
 
     useEffect(()=>{
         if(location.state){
